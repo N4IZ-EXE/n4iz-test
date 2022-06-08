8af0485c4d24e054e6028a2001ccda8e1bf08846
N4IZ-EXE <86039735+N4IZ-EXE@users.noreply.github.com>
2022-06-08 22:33:22 +0600

    Create test.py

---
+++ test.py
@@ -0,0 +1,31 @@
+def logo():
+
+    os.system("clear")
+
+    print('''
+
+    _   _ _  _  ___ _____
+
+   | \ | | || ||_ _|__  /
+
+   |  \| | || |_| |  / /
+
+   | |\  |__   _| | / /_
+
+   |_| \_|  |_||___/____|
+
+   '''
+
+   )
+
+    print("\033[94m│                              \033[94m          │".center(columns+9))
+
+    print("\033[94m│ \033[95mAuthor : N4IZ                     \033[94m│".center(columns+15))
+
+    print("│ \033[95mTool   : SMS Bomber          \033[94m│".center(columns+9))
+
+    print("│ \033[95mGitHub : https://github.com/N4IZ \033[94m│".center(columns+9))
+
+    print("│ \033[95mCoder  : Nafiz Ahmed             \033[37mV3.0 \033[94m│".center(columns+15))
+
+    print("\033[94m└────────────────────────────────────────┘".center(columns+5))
